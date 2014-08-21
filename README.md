5f7b1727fa228416c53ec251b66904441d8d6b01
Luiz Freneda <lfreneda@gmail.com>
2014-08-21 15:41:08 -0300

    Configuration hotfix

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ GzipS3Client
 Lightweight AWS S3 Client. Simplify usage of AWS S3 to get and put objects.
 
 Its also zip/unzip before send/get data to/from S3, so you can save space storage, in other words, so you can save $$ monthly :-)
+
+
--- src/GzipS3Client/Configuration/AppSettingsAmazonS3Configuration.cs
+++ src/GzipS3Client/Configuration/AppSettingsAmazonS3Configuration.cs
@@ -2,7 +2,7 @@ using System.Configuration;
 
 namespace GzipS3Client.Configuration
 {
-    public class AppSettingsAmazonS3Configuration : AppSettingsAmazonConfiguration
+    public class AppSettingsAmazonS3Configuration : AppSettingsAmazonConfiguration, IAmazonS3Configuration
     {
         public string ServiceUrl { get { return ConfigurationManager.AppSettings["aws.serviceUrl"]; } }
         public string BucketName { get { return ConfigurationManager.AppSettings["aws.bucketName"]; } }
